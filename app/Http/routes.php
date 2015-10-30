342ccaa46ff76b9cd16eb98c9a2d6c14fb7e6763
Kevin van der Burgt <kevin@zdev.com>
2015-10-30 22:21:20 +0100

    Added post request to search

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['namespace' => 'Api', 'as' => 'api::', 'middleware' => []], function () {
+Route::group(['namespace' => 'Api', 'prefix' => 'api', 'as' => 'api::', 'middleware' => []], function () {
     Route::post('/search', [
         'uses' => 'SearchController@postSearch',
         'as' => 'search',
