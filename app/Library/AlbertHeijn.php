3a0605760f8ca8ab7ad9b5b10ea027e36f8ee7d4
Timo de Jong <timodejong95@gmail.com>
2015-10-31 00:51:02 +0100

    Added functionality for searching recipes by ingredient Added documentation

--- app/Library/AlbertHeijn.php
+++ app/Library/AlbertHeijn.php
@@ -6,6 +6,14 @@ use Ixudra\Curl\Facades\Curl;
 
 class AlbertHeijn
 {
+    /**
+     * @var string $_baseUrl The base url to the Albert Heijn Api
+     */
+    private static $_baseUrl = 'https://frahmework.ah.nl/ah';
+
+    /**
+     * @var string $_apiKey The Albert Heijn Api key
+     */
     private static $_apiKey = '';
 
     public static function apiKey($apiKey)
@@ -21,13 +29,31 @@ class AlbertHeijn
      */
     public static function products($term)
     {
-        $response =  Curl::to('https://frahmework.ah.nl/ah/json/producten')
+        $response =  Curl::to(self::$_baseUrl.'/json/producten')
             ->withData([
                 'productomschrijving' => $term,
-                'personalkey' => self::$_apiKey
+                'personalkey' => self::$_apiKey,
             ])
             ->get();
 
         dd($response);
+    }
+
+    /**
+     * Returns recipes where the ingredient stands in
+     *
+     * @param string $ingredient Ingredient which we want to search in recipes
+     * @return object JSON object with results
+     */
+    public static function recipes($ingredient)
+    {
+        $response =  Curl::to(self::$_baseUrl.'/json/recepten')
+            ->withData([
+                'receptomschrijving' => $ingredient,
+                'personalkey' => self::$_apiKey,
+            ])
+            ->get();
+
+        return $response;
     }
 }
